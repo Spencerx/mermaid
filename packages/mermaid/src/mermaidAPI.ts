Spencerx/mermaid

commit 64769738d5b59211e1decb471ffbaca8afec51aa

fix: prevent CSS namespace escape using `:not(&)`
Currently, we're namespacing user's custom CSS to only apply within
the `<div id='svgId'>` by doing:

```css
 #svgId {
  .myCssClass { /* my rules here */ }
}
```

If there's no `&` in the child rule selector, one gets prefixed
[automatically][1], which namespaces the rule correctly.

```css
 #svgId {
  & .myCssClass { /* my rules here */}
}
```

However, if an `&` is present in the child rule selector, there's no
automatic prefix of the parent rule selector, which allows user defined
CSS to escape the `<div id='svgId'>`, e.g.

```css
 #svgId {
  :not(&) { /* my rules here */ }
}
```

This commit adds a stylis middleware that automatically prefixes
`#svgId` to any rule selector if it's not already there, preventing this
bypass.

[1]: https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Selectors/Nesting_selector

packages/mermaid/src/mermaidAPI.ts
@@ -4,7 +4,7 @@
  */
 // @ts-ignore TODO: Investigate D3 issue
 import { select } from 'd3';
-import { compile, serialize, stringify } from 'stylis';
+import { compile, middleware, serialize, stringify } from 'stylis';
 import DOMPurify from 'dompurify';
 import isEmpty from 'lodash-es/isEmpty.js';
 import { addSVGa11yTitleDescription, setA11yDiagramInfo } from './accessibility.js';
@@ -188,6 +188,50 @@ export const createCssStyles = (
   return cssString + cssStyleSheetToString(cssStyles);
 };
 
+/**
+ * Use `stylis` to compile the CSS to only apply to the given namespace.
+ *
+ * This will also remove some newer CSS features (e.g. nesting) to better
+ * support older browsers and does some minification.
+ *
+ * @internal
+ * @param namespace - the namespace to add in front of all the CSS styles, e.g. `#idOfSvgElement`
+ * @param css - the CSS styles to add the namespace to.
+ * @see https://github.com/thysultan/stylis
+ *
+ * @example
+ * // Returns `#id .class1{fill:red;}`
+ * compileCSS('#id', `.class1 { fill: red }`)
+ */
+const compileCSS = (namespace: `#${string}`, css: string) => {
+  return serialize(
+    compile(`${namespace}{${css}}`),
+    middleware([
+      function addNamespace(element, _index, _children, _callback) {
+        /**
+         * CSS normally automatically adds the `&` selector in front of each
+         * element. But, if there's already an `&` selector, it doesn't add this.
+         *
+         * This code will explicitly make sure it's always added, to ensure
+         * that the CSS never applies outside the SVG.
+         *
+         * E.g. `#svgId { .nested-class :not(&) { fill: red } }` will be
+         * transformed to `#svgId { & .nested-class :not(&) { fill: red } }`
+         */
+        if (element.type === 'rule' && Array.isArray(element.props)) {
+          element.props = element.props.map((prop) => {
+            if (!prop.startsWith(namespace)) {
+              return `${namespace} ${prop}`;
+            }
+            return prop;
+          });
+        }
+      },
+      stringify,
+    ])
+  );
+};
+
 export const createUserStyles = (
   config: MermaidConfig,
   graphType: string,
@@ -202,11 +246,7 @@ export const createUserStyles = (
     { ...config.themeVariables, theme: config.theme, look: config.look },
     svgId
   );
-
-  // Now turn all of the styles into a (compiled) string that starts with the id
-  // use the stylis library to compile the css, turn the results into a valid CSS string (serialize(...., stringify))
-  // @see https://github.com/thysultan/stylis
-  return serialize(compile(`${svgId}{${allStyles}}`), stringify);
+  return compileCSS(svgId, allStyles);
 };
 
 /**

packages/mermaid/src/mermaidAPI.spec.ts
@@ -461,6 +461,21 @@ describe('mermaidAPI', () => {
         '#someId .edge-pattern-dashed{stroke-dasharray:3;}#someId .default{color:red;}#someId .classDef2>*{color:purple;}#someId .classDef2 span{color:purple;}'
       );
     });
+
+    it('should handle `:not(&)` selectors in the CSS', () => {
+      const result = createUserStyles(
+        {
+          ...mockConfig,
+          themeCSS: ':not(&){background:green !important}',
+        },
+        'someDiagram',
+        new Map(),
+        '#someId'
+      );
+      expect(result).toEqual(
+        '#someId .edge-pattern-dashed{stroke-dasharray:3;}#someId :not(#someId){background:green!important;}'
+      );
+    });
   });
 
   describe('removeExistingElements', () => {

cypress/integration/other/ghsa.spec.js
@@ -41,4 +41,18 @@ describe('CSS injections', () => {
     );
     cy.get('body > div #pwned').should('not.exist');
   });
+  it('should prevent CSS namespace injection via :not(&)', () => {
+    imgSnapshotTest(
+      `---
+title: Green background CSS should not be able to escape the diagram using :not(&)
+config:
+  themeCSS: ':not(&){background:green !important}'
+---
+flowchart
+  A --> B
+     `,
+      { logLevel: 1 }
+    );
+    cy.get('body').should('not.have.css', 'background-color', 'rgb(0, 128, 0)');
+  });
 });

commit ba3daef40dec0ede5ff0aebe88ff591079e33550

fix: skip namespacing CSSKeyframeRule
This was breaking animations, since we changing

```css
@keyframes hi {
  from {
    stroke-dashoffset:1000;
  }
}
```

to

```css
@keyframes hi {
  /* Not correct */
  #svgId from {
    stroke-dashoffset:1000;
  }
}
```

Fixes: 64769738d5b59211e1decb471ffbaca8afec51aa

packages/mermaid/src/mermaidAPI.ts
@@ -231,6 +231,12 @@ const compileCSS = (namespace: `#${string}`, css: string) => {
          * transformed to `#svgId { & .nested-class :not(&) { fill: red } }`
          */
         if (element.type === 'rule' && Array.isArray(element.props)) {
+          if (element.parent && element.parent.type === KEYFRAMES) {
+            /**
+             * Don't namespace CSSKeyframeRule, since they don't have selectors.
+             */
+            return;
+          }
           element.props = element.props.map((prop) => {
             if (!prop.startsWith(namespace)) {
               return `${namespace} ${prop}`;

packages/mermaid/src/mermaidAPI.spec.ts
@@ -504,6 +504,21 @@ describe('mermaidAPI', () => {
         '#someId .edge-pattern-dashed{stroke-dasharray:3;}@media (max-width: 600px){#someId *{background-color:lightblue;}}@supports selector(h2 > p){#someId h2>p{color:red;}}'
       );
     });
+
+    it('should not namespace keyframe rules', () => {
+      const result = createUserStyles(
+        {
+          ...mockConfig,
+          themeCSS: '@keyframes dash { to { stroke-dashoffset: 1000; } }',
+        },
+        'someDiagram',
+        new Map(),
+        '#someId'
+      );
+      expect(result).toEqual(
+        '#someId .edge-pattern-dashed{stroke-dasharray:3;}@keyframes dash{to{stroke-dashoffset:1000;}}'
+      );
+    });
   });
 
   describe('removeExistingElements', () => {